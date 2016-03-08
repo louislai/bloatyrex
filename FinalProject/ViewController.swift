9b8d8faf142f36815f61378d59f481512be5d069
Tham Zheng Yi <thamzhengyi@gmail.com>
2016-03-08 21:09:40 +0800

    Initial commit.

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Tham Zheng Yi on 8/3/16.
+//  Copyright © 2016 nus.cs3217.2016Group6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
