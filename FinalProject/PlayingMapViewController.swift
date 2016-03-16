0ce47fcd24d4375b5b31a98b4d8b9cb13ba1905c
Louis Lai <hoangdung2506@gmail.com>
2016-03-17 00:56:15 +0800

    Add in basic controller flow

---
+++ FinalProject/PlayingMapViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Tham Zheng Yi on 8/3/16.
+//  Copyright © 2016 nus.cs3217.2016Group6. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class PlayingMapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Configure the view.
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+
+        // Present the scene.
+        skView.presentScene(newScene())
+    }
+
+    func newScene() -> PlayingMapScene {
+        let skView = view as! SKView
+
+        // Create and configure the scene.
+        let scene = PlayingMapScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+
+        // Load the map
+        let map = Map(numberOfRows: 3, numberOfColumns: 9)
+        map.setMapUnitAt(.Agent, row: 0, column: 0)
+        map.setMapUnitAt(.Wall, row: 0, column: 1)
+        map.setMapUnitAt(.Goal, row: 0, column: 7)
+        for i in 0..<9 {
+            map.setMapUnitAt(.Wall, row: 2, column: i)
+        }
+        for i in 0..<3 {
+            map.setMapUnitAt(.Wall, row: i, column: 8)
+        }
+        scene.map = map
+        scene.resetDelegate = self
+        scene.setup()
+
+        return scene
+    }
+}
+
+extension PlayingMapViewController: ResetDelegate {
+    func reset() {
+        let skView = view as! SKView
+
+        let transition = SKTransition.crossFadeWithDuration(0.5)
+        skView.presentScene(newScene(), transition: transition)
+    }
+}
