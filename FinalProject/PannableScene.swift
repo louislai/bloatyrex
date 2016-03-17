ef3583e7f21d98f3716d058acd2dd3b737bfbcbe
Tham Zheng Yi <thamzhengyi@gmail.com>
2016-03-17 12:27:22 +0800

    Created the empty PannableScene class.

---
+++ FinalProject/PannableScene.swift
@@ -0,0 +1,14 @@
+//
+//  PannableScene.swift
+//  FinalProject
+//
+//  Created by Tham Zheng Yi on 17/3/16.
+//  Copyright © 2016 nus.cs3217.2016Group6. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class PannableScene: SKScene {
+
+}
