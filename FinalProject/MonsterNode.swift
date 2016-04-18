9d528aae400ae4e1c125e340bea0b40e0ee6c698
Louis Lai <hoangdung2506@gmail.com>
2016-04-18 22:32:43 +0800

    Add zzz animation for monster

--- FinalProject/MonsterNode.swift
+++ FinalProject/MonsterNode.swift
@@ -12,8 +12,9 @@ class MonsterNode: MapUnitNode {
     var frequencyMin = 2
     var frequencyMax = 2
     var turnsUntilAwake = 0
-
-
+    var zzzOn = false
+    let zzzNode = SKSpriteNode(texture: TextureManager.retrieveTexture("zzz"))
+    let timePerAnimation: NSTimeInterval = 0.15
 
     required init(type: MapUnitType = .Hole) {
         super.init(type: .Monster)
@@ -48,7 +49,9 @@ class MonsterNode: MapUnitNode {
     }
 
     func setOrientation(orientation: Direction) {
+        setAwake()
         xScale = 1
+        zzzNode.xScale = 1
         switch orientation {
         case .Up:
             texture = TextureManager.monsterUpTexture
@@ -56,6 +59,7 @@ class MonsterNode: MapUnitNode {
             texture = TextureManager.monsterDownTexture
         case .Left:
             xScale = -1
+            zzzNode.xScale = -1
             fallthrough
         case .Right:
             texture = TextureManager.monsterRightTexture
@@ -77,9 +81,43 @@ class MonsterNode: MapUnitNode {
 
     func setAwake() {
         texture = TextureManager.monsterDownTexture
+        zzzNode.removeAllActions()
+        zzzNode.removeFromParent()
+        zzzOn = false
     }
 
     func setSleeping() {
         texture = TextureManager.monsterSleepingTexture
+        zzzNode.zPosition = GlobalConstants.zPosition.front
+        if !zzzOn {
+            let bWidth = GlobalConstants.Dimension.blockWidth
+            let bHeight = GlobalConstants.Dimension.blockHeight
+            let smallSize = CGSize(
+                width: bWidth*0.5,
+                height: bHeight*0.5
+            )
+            zzzNode.size = smallSize
+            let resizeSmallAction = SKAction.resizeToWidth(
+                bWidth*0.5,
+                height: bHeight*0.5,
+                duration: timePerAnimation
+            )
+            resizeSmallAction.timingMode = .EaseIn
+            let resizeBigAction = SKAction.resizeToWidth(
+                bWidth,
+                height: bHeight,
+                duration: timePerAnimation
+            )
+            resizeBigAction.timingMode = .EaseOut
+            let actionSequence = SKAction.repeatActionForever(
+                SKAction.sequence([
+                    resizeSmallAction,
+                    resizeBigAction
+                    ])
+            )
+            addChild(zzzNode)
+            zzzNode.runAction(actionSequence)
+            zzzOn = true
+        }
     }
 }
