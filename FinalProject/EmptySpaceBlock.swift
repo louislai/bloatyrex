edc50e808d90220f013eef39fdac06cec92e489f
Louis Lai <hoangdung2506@gmail.com>
2016-04-19 21:09:31 +0800

    Linting

--- FinalProject/EmptySpaceBlock.swift
+++ FinalProject/EmptySpaceBlock.swift
@@ -10,7 +10,7 @@ import SpriteKit
 
 class EmptySpaceBlock: ObjectBlock {
     let blockBody: SKSpriteNode
-    
+
     override init(containingBlock: ContainerBlockProtocol, containingZone: DropZone) {
         blockBody = SKSpriteNode(imageNamed: "space")
         blockBody.size = CGSize(width: 64, height: 64)
@@ -18,13 +18,12 @@ class EmptySpaceBlock: ObjectBlock {
         super.init(containingBlock: containingBlock, containingZone: containingZone)
         addChild(blockBody)
     }
-    
+
     override func getMapUnit() -> MapUnitType {
         return MapUnitType.EmptySpace
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
--- FinalProject/MonsterBlock.swift
+++ FinalProject/MonsterBlock.swift
@@ -10,7 +10,7 @@ import SpriteKit
 
 class MonsterBlock: ObjectBlock {
     let blockBody: SKSpriteNode
-    
+
     override init(containingBlock: ContainerBlockProtocol, containingZone: DropZone) {
         blockBody = SKSpriteNode(imageNamed: "monster-static")
         blockBody.size = CGSize(width: 64, height: 64)
@@ -18,13 +18,12 @@ class MonsterBlock: ObjectBlock {
         super.init(containingBlock: containingBlock, containingZone: containingZone)
         addChild(blockBody)
     }
-    
+
     override func getMapUnit() -> MapUnitType {
         return MapUnitType.Monster
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
