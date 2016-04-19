f6c7c80cdd1e3099819607ad85576c447e346997
Koh Wai Kit <kwkit1992@gmail.com>
2016-04-20 01:39:38 +0800

    Refactor out a redundant enum

--- FinalProject/BlockButton.swift
+++ FinalProject/BlockButton.swift
@@ -8,49 +8,76 @@
 
 import SpriteKit
 
-enum BlockType {
-    case Main
-    case Forward
-    case TurnLeft
-    case TurnRight
-    case While
-    case Eyes
-    case Toilet
-    case Wall
-    case Hole
-    case Wood
-    case If
-    case Not
-    case Safe
-    case Wait
-    case Jump
-    case PressBlue
-    case PressRed
-    case LeftCorrect
-    case RightCorrect
-    case Empty
-    case Monster
-}
-
 class BlockButton: SKNode {
     let button: SKSpriteNode
     let block: SKSpriteNode
-    let blockType: BlockType
+    private var actionFunc: ((ContainerBlockProtocol) -> CodeBlock)?
+    private var boolOpFunc: ((ContainerBlockProtocol, DropZone) -> BoolOpBlock)?
+    private var objectFunc: ((ContainerBlockProtocol, DropZone) -> ObjectBlock)?
     let blockCategory: BlockCategory
     var selected = false
 
-    init(imageNamed: String, blockType: BlockType, blockCategory: BlockCategory) {
+    init(imageNamed: String, blockCategory: BlockCategory, action: (ContainerBlockProtocol) -> CodeBlock) {
         button = SKSpriteNode(imageNamed: imageNamed)
         block = SKSpriteNode(imageNamed: imageNamed)
         let size = GlobalConstants.CodeBlocks.blockSize
         button.size = CGSize(width: size, height: size)
         block.size = CGSize(width: size, height: size)
         block.hidden = true
-        self.blockType = blockType
+        self.actionFunc = action
         self.blockCategory = blockCategory
         super.init()
         self.addChild(button)
         self.addChild(block)
+    }
+    
+    init(imageNamed: String, blockCategory: BlockCategory, object: (ContainerBlockProtocol, DropZone) -> ObjectBlock) {
+        button = SKSpriteNode(imageNamed: imageNamed)
+        block = SKSpriteNode(imageNamed: imageNamed)
+        let size = GlobalConstants.CodeBlocks.blockSize
+        button.size = CGSize(width: size, height: size)
+        block.size = CGSize(width: size, height: size)
+        block.hidden = true
+        self.objectFunc = object
+        self.blockCategory = blockCategory
+        super.init()
+        self.addChild(button)
+        self.addChild(block)
+    }
+    
+    init(imageNamed: String, blockCategory: BlockCategory, boolOp: (ContainerBlockProtocol, DropZone) -> BoolOpBlock) {
+        button = SKSpriteNode(imageNamed: imageNamed)
+        block = SKSpriteNode(imageNamed: imageNamed)
+        let size = GlobalConstants.CodeBlocks.blockSize
+        button.size = CGSize(width: size, height: size)
+        block.size = CGSize(width: size, height: size)
+        block.hidden = true
+        self.boolOpFunc = boolOp
+        self.blockCategory = blockCategory
+        super.init()
+        self.addChild(button)
+        self.addChild(block)
+    }
+    
+    func getAction(container: ContainerBlockProtocol) -> CodeBlock? {
+        if let fun = actionFunc {
+            return fun(container)
+        }
+        return nil
+    }
+    
+    func getObject(container: ContainerBlockProtocol, zone: DropZone) -> ObjectBlock? {
+        if let fun = objectFunc {
+            return fun(container, zone)
+        }
+        return nil
+    }
+    
+    func getBoolOp(container: ContainerBlockProtocol, zone: DropZone) -> BoolOpBlock? {
+        if let fun = boolOpFunc {
+            return fun(container, zone)
+        }
+        return nil
     }
 
     func pickBlock(selected: Bool, scale: CGFloat) {
