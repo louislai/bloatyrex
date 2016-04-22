0570f6b6c65b7cd6425213cb57842fa68bf89335
Melvin Tan Jun Keong <melvin@r-187-102-25-172.comp.nus.edu.sg>
2016-04-22 10:38:59 +0800

    Page View Controller constants

--- FinalProject/LevelSelectorPageViewControllerConstants.swift
+++ FinalProject/LevelSelectorPageViewControllerConstants.swift
@@ -6,9 +6,15 @@
 //  Copyright © 2016 nus.cs3217.2016Group6. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
 
 struct LevelSelectorPageViewControllerConstants {
     static let headerText = "Tap to load level, Press-and-hold for Settings"
     static let searchingText = "Search for file name..."
+    
+    struct Position {
+        static let searching = CGRect(x: 0, y: 0, width: 1024, height: 60)
+        static let navigation = CGRect(x: 0, y: 60, width: 1024, height: 50)
+        static let pageController = CGRect(x: 0, y: 60, width: 1024, height: 708)
+    }
 }
